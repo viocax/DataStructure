942a671a3fde741952be424329e32312ea7e0b96
Drake <drake.huang@lollipop.camera>
2024-08-19 14:48:08 +0800

    Initial Commit

---
+++ DataStructure/DataStructure.h
@@ -0,0 +1,18 @@
+//
+//  DataStructure.h
+//  DataStructure
+//
+//  Created by drake on 2024/8/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataStructure.
+FOUNDATION_EXPORT double DataStructureVersionNumber;
+
+//! Project version string for DataStructure.
+FOUNDATION_EXPORT const unsigned char DataStructureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DataStructure/PublicHeader.h>
+
+
---
+++ DataStructureTests/DataStructureTests.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructureTests.swift
+//  DataStructureTests
+//
+//  Created by drake on 2024/8/19.
+//
+
+import Testing
+@testable import DataStructure
+
+struct DataStructureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
